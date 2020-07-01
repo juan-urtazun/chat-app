cd2ed707fd09c2deacfb75bdc75ba84236858519
Author Name <juan-urtazun@users.noreply.github.com>
2020-07-01 15:52:37 -0300

    feat(client): add InfoBar component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,26 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
 
 let socket;
-const Chat = ({location}) =>{
+const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const ENDPOINT = 'localhost:5000';
-  useEffect(()=>{
-    const {name, room} = queryString.parse(location.search);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
     setName(name);
     setRoom(room);
     socket = io(ENDPOINT);
-    socket.emit('join', {name, room}, ()=>{});
+    socket.emit("join", { name, room }, () => {});
 
-    
-    return ()=>{
-      socket.emit('disconnect');
+    return () => {
+      socket.emit("disconnect");
       socket.off();
-    }
+    };
   }, [ENDPOINT, location.search]);
-  return <h1>chat</h1>
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event)=>{
+    event.preventDefault();
+
+    if(message){
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
+
+  console.log(messages, message)
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room}/>
+ 
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
