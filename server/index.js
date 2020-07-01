59586b50f6723ab1d8b98fd5822c9831605cc6c1
Author Name <juan-urtazun@users.noreply.github.com>
2020-07-01 14:59:18 -0300

    feat(server): add feature join user

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const socket_io = require('socket.io');
-const http = require('http');
+const express = require("express");
+const socket_io = require("socket.io");
+const http = require("http");
 
-const router = require('./router');
+const { addUser, removeUser, getUser, getUserInRoom } = require("./users");
+
+const router = require("./router");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -11,20 +14,32 @@ const io = socket_io(server);
 
 app.use(router);
 
-io.on('connection', (socket)=>{
-    console.log('We have a new connection!!!');
+io.on("connection", (socket) => {
+  console.log("We have a new connection!!!");
 
-    socket.on('join', ({name, room}, callback)=>{
-        console.log('Join',name, room);
+  socket.on("join", ({ name, room }, callback) => {
+    const { user, error } = addUser({ id: socket.id, name, room });
 
-        callback();
+    if (error) return callback(error);
 
-    })
-    socket.on('disconnect', ()=>{
-        console.log('User had left!');
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
     });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User had left!");
+  });
 });
 
-server.listen(PORT, ()=>{
-    console.log(`Server has started on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
