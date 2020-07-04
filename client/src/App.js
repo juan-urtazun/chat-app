207b875664bc6ddb794c60c85d48c2ee978a82bb
Juan Urtazun <juan-urtazun@users.noreply.github.com>
2020-07-04 15:24:10 -0300

    feat(App): add normalize.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
+import "normalize.css";
+
 const App = () =>(
     <Router>
         <Route  path="/" exact component={Join}/>
